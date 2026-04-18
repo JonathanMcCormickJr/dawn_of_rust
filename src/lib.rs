26b67abe3837a707cb8e724453d61e4c5af3c909
Jonathan McCormick <67705789+JonathanMcCormickJr@users.noreply.github.com>
2026-04-18 12:49:44 -0500

    adding test test

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+/// My example fuction
+/// ```rust
+/// use dawn_of_rust::my_func;
+/// my_func(5);
+/// ```
+pub fn my_func(num: i32) {
+    println!("{}", num);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn my_test() {
+        assert_eq!(1, 1);
+    }
+}
